d05230f8fee6a045f6552d856ddbbaade139f160
Kenny Aragón <kenny.ara.cas.ka@gmail.com>
2024-01-27 20:07:08 -0600

    Avance1

---
+++ Tarea1/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Guna2Panel2 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
+        Me.ComboBoxGender = New Guna.UI2.WinForms.Guna2ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBoxAnalysis = New Guna.UI2.WinForms.Guna2ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BSubmit = New Guna.UI2.WinForms.Guna2Button()
+        Me.Guna2Panel2.SuspendLayout()
+        Me.Guna2Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.BlueViolet
+        Me.Label1.Location = New System.Drawing.Point(26, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(127, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Blood meter"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Guna2Panel2
+        '
+        Me.Guna2Panel2.BackColor = System.Drawing.SystemColors.Control
+        Me.Guna2Panel2.Controls.Add(Me.Guna2Panel1)
+        Me.Guna2Panel2.Controls.Add(Me.Label1)
+        Me.Guna2Panel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Guna2Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Guna2Panel2.Name = "Guna2Panel2"
+        Me.Guna2Panel2.Size = New System.Drawing.Size(537, 541)
+        Me.Guna2Panel2.TabIndex = 2
+        '
+        'Guna2Panel1
+        '
+        Me.Guna2Panel1.BackColor = System.Drawing.Color.BlueViolet
+        Me.Guna2Panel1.Controls.Add(Me.BSubmit)
+        Me.Guna2Panel1.Controls.Add(Me.ComboBoxGender)
+        Me.Guna2Panel1.Controls.Add(Me.Label3)
+        Me.Guna2Panel1.Controls.Add(Me.ComboBoxAnalysis)
+        Me.Guna2Panel1.Controls.Add(Me.Label2)
+        Me.Guna2Panel1.Location = New System.Drawing.Point(31, 76)
+        Me.Guna2Panel1.Name = "Guna2Panel1"
+        Me.Guna2Panel1.Size = New System.Drawing.Size(175, 453)
+        Me.Guna2Panel1.TabIndex = 2
+        '
+        'ComboBoxGender
+        '
+        Me.ComboBoxGender.BackColor = System.Drawing.Color.Transparent
+        Me.ComboBoxGender.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ComboBoxGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxGender.FocusedColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ComboBoxGender.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ComboBoxGender.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.ComboBoxGender.ForeColor = System.Drawing.Color.FromArgb(CType(CType(68, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.ComboBoxGender.ItemHeight = 30
+        Me.ComboBoxGender.Location = New System.Drawing.Point(21, 153)
+        Me.ComboBoxGender.Name = "ComboBoxGender"
+        Me.ComboBoxGender.Size = New System.Drawing.Size(140, 36)
+        Me.ComboBoxGender.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label3.Location = New System.Drawing.Point(28, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 16)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Gender"
+        '
+        'ComboBoxAnalysis
+        '
+        Me.ComboBoxAnalysis.BackColor = System.Drawing.Color.Transparent
+        Me.ComboBoxAnalysis.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.ComboBoxAnalysis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxAnalysis.FocusedColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ComboBoxAnalysis.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ComboBoxAnalysis.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.ComboBoxAnalysis.ForeColor = System.Drawing.Color.FromArgb(CType(CType(68, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(112, Byte), Integer))
+        Me.ComboBoxAnalysis.ItemHeight = 30
+        Me.ComboBoxAnalysis.Location = New System.Drawing.Point(21, 54)
+        Me.ComboBoxAnalysis.Name = "ComboBoxAnalysis"
+        Me.ComboBoxAnalysis.Size = New System.Drawing.Size(140, 36)
+        Me.ComboBoxAnalysis.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label2.Location = New System.Drawing.Point(18, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Analysis type"
+        '
+        'BSubmit
+        '
+        Me.BSubmit.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.BSubmit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.BSubmit.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.BSubmit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.BSubmit.FillColor = System.Drawing.Color.Indigo
+        Me.BSubmit.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.BSubmit.ForeColor = System.Drawing.Color.White
+        Me.BSubmit.Location = New System.Drawing.Point(31, 237)
+        Me.BSubmit.Name = "BSubmit"
+        Me.BSubmit.Size = New System.Drawing.Size(91, 29)
+        Me.BSubmit.TabIndex = 5
+        Me.BSubmit.Text = "Submit"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(537, 541)
+        Me.Controls.Add(Me.Guna2Panel2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Guna2Panel2.ResumeLayout(False)
+        Me.Guna2Panel2.PerformLayout()
+        Me.Guna2Panel1.ResumeLayout(False)
+        Me.Guna2Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Guna2Panel2 As Guna.UI2.WinForms.Guna2Panel
+    Friend WithEvents Guna2Panel1 As Guna.UI2.WinForms.Guna2Panel
+    Friend WithEvents ComboBoxGender As Guna.UI2.WinForms.Guna2ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBoxAnalysis As Guna.UI2.WinForms.Guna2ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BSubmit As Guna.UI2.WinForms.Guna2Button
+End Class
---
+++ Tarea1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBoxAnalysis.Items.Add("Red blood cells")
+        ComboBoxAnalysis.Items.Add("Hemoglobin")
+        ComboBoxAnalysis.Items.Add("Hematocrit")
+        ComboBoxAnalysis.Items.Add("Leukocytes")
+        ComboBoxAnalysis.Items.Add("MCV")
+        ComboBoxAnalysis.Items.Add("MCHC")
+        ComboBoxAnalysis.Items.Add("Platelets")
+
+        ComboBoxGender.Items.Add("Male")
+        ComboBoxGender.Items.Add("Female")
+
+
+    End Sub
+
+    Private Sub ComboBoxAnalysis_SelectedIndexChanged(sender As Object, e As EventArgs)
+        ' aquí se pone el evento del primer combo box
+    End Sub
+
+    Private Sub ComboBoxGender_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+End Class
